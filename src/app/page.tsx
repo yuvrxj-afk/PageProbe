4cc4c97cf2e1e27ab474fd81281dac6026b8e18b
yuvrxj <yuvrajsisodiya7302@gmail,com>
2023-10-15 22:43:04 +0530

    Landing Page and ui.shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,22 @@ export default function Home() {
        overflow-hidden rounded-full border border-gray-200 bg-white 
        px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50"
       >
-        hello world
+        <p className="text-sm font-semibold text-gray-700">
+          PageProbe is here!
+        </p>
       </div>
+      <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+        <span className="text-blue-600 font-extrabold"> Text </span> to
+        <span className="text-blue-600 font-extrabold"> Conversation</span>
+        <br />
+        in Seconds.
+      </h1>
+      <p className="mt-5 max-w-prose text-zinc-600 sm:text-lg place-self-start">
+        Experience PageProbe: Effortlessly Engage in Conversations with PDF
+        Documents. Begin by Uploading Your File and Instantly Chatting with
+        <span className="text-zinc-800 font-bold "> Martin </span>
+        the bot.
+      </p>
     </MaxWidthWrapper>
   );
 }
