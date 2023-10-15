eab113093717b7d546e3e24adcfe0ac5e5906339
yuvrxj <yuvrajsisodiya7302@gmail,com>
2023-10-15 23:46:52 +0530

    gradient / ui components

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
-import clsx, { ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs:ClassValue[]){
-    // px-2 py-2 => p-2
-    return twMerge(clsx(inputs))
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
