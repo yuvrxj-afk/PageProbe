1ccb221954d267381af8bc2bc45e6f3b9b2b7929
yuvrxj <yuvrajsisodiya7302@gmail,com>
2023-10-17 01:12:43 +0530

    tRPC client / prisma setup

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,58 @@
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { buttonVariants } from "./ui/button";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import { ArrowRightCircleIcon } from "lucide-react";
+import Image from "next/image";
+
+const NavBar = () => {
+  return (
+    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+          <Link href="/" className="font-semibold text-lg flex z-40">
+            <Image
+              src="/PageProbe-logo.png"
+              alt="logo"
+              width={180}
+              height={90}
+              className="drop-shadow-sm"
+            />
+          </Link>
+
+          {/* todo : add mobile navBar */}
+
+          <div className="hidden items-center space-x-4 sm:flex">
+            <>
+              <Link
+                href="/pricing"
+                className={buttonVariants({
+                  variant: "ghost",
+                  size: "sm",
+                })}
+              >
+                Pricing
+              </Link>
+              <LoginLink
+                className={buttonVariants({
+                  variant: "ghost",
+                  size: "sm",
+                })}
+              >
+                Log in
+              </LoginLink>
+              <RegisterLink
+                className={buttonVariants({
+                  size: "sm",
+                })}
+              >
+                Get Started <ArrowRightCircleIcon className="ml-1.5 h-5 w-5" />
+              </RegisterLink>
+            </>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
+export default NavBar;
---
+++ src/trpc/index.ts
@@ -0,0 +1,17 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { publicProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
+export const appRouter = router({
+  authCallback: publicProcedure.query(() => {
+    const { getUser } = getKindeServerSession();
+    const user = getUser();
+
+    if (!user.id || !user.email) {
+      throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
+    return { success: true };
+  }),
+});
+// Export type router type signature,
+// NOT the router itself.
+export type AppRouter = typeof appRouter;
