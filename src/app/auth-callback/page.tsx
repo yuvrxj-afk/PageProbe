567cf316a3958b9435d0365f264e4d1e5d868fd5
yuvrxj <yuvrajsisodiya7302@gmail,com>
2023-10-17 15:02:52 +0530

    db sync  | auth-callback

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { useRouter, useSearchParams } from "next/navigation";
 import { trpc } from "../_trpc/client";
+import { Loader2 } from "lucide-react";
 
-const Page = async () => {
+const Page =  () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
@@ -12,7 +15,23 @@ const Page = async () => {
         router.push(origin ? `/${origin}` : "/dashboard");
       }
     },
+    onError: (err) => {
+      if (err.data?.code === "UNAUTHORIZED") {
+        router.push("/sign-in");
+      }
+    },
+    retry: true,
+    retryDelay: 500,
   });
+  return (
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semi-bold text-xl">Setting up your account...</h3>
+        <p>You will be redirected automatically.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
