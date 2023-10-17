8fb90656f8edd3497cf70f789ed99802e0512f5b
yuvrxj <yuvrajsisodiya7302@gmail,com>
2023-10-17 16:28:15 +0530

    dialogbox / upload func

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,17 @@
-const Dashboard = ({ email }: any) => {
-  return <div>hello {email} its dashboard</div>;
+import UploadButton from "./UploadButton";
+
+const Dashboard = () => {
+  return (
+    <main className="mx-auto max-w-7xl md:p-10">
+      <div
+        className="mt-8 flex flex-row items-start justify-between gap-4 border-b-2 border-gray-500 pb-3 px-2 sm:items-center sm:gap-0 "
+      >
+        <h1 className="text-5xl mb-3 font-bold text-gray-900">My Files</h1>
+        <UploadButton />
+
+      </div>
+    </main>
+  );
 };
 
 export default Dashboard;
