4d421b3d60cba7c0738d560d7b53c8441efbac46
yuvrxj <yuvrajsisodiya7302@gmail,com>
2023-10-30 18:09:54 +0530

    context: validator

---
+++ src/app/api/message/route.ts
@@ -0,0 +1,33 @@
+import { db } from "@/db";
+import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  // endpoint to ask from pdf
+
+  const body = await req.json();
+
+  const { getUser } = getKindeServerSession();
+
+  const user = getUser();
+
+  const { id: userId } = user;
+
+  if (!userId) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  const { fileId, message } = SendMessageValidator.parse(body);
+
+  const file = await db.file.findFirst({
+    where:{
+        id:fileId,
+        userId
+    }
+  })
+
+  if(!file){
+    return new Response('Not Found')
+  }
+};
